711c74226bda3b641f7ca5a9f85773ae8f271676
Mesut <yildirimmesutyigit@gmail.com>
2023-06-05 22:08:59 +0300

    new commit

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ const selectionArticle = document.querySelector(".selection");
 const yourChoiceDiv = document.getElementById("your-choice");
 const pcChoiceDiv = document.getElementById("pc-choice");
 
+//? message
+const messagePar = document.querySelector(".message");
+
+//? Score
+const scoreCardSection = document.querySelector(".score-card");
+
 //* ------- Variables ------- */
 let userSelectImg = document.createElement("img");
 let pcSelectImg = document.createElement("img");
+
+//? Colors
+const YELLOW = "#ffc538";
+const RED = "#fb778b";
+const GREEN = "#5ab7ac";
 
 //* ------- Event Listeners ------- */
 selectionArticle.addEventListener("click", (e) => {
@@ -20,14 +31,46 @@ selectionArticle.addEventListener("click", (e) => {
   }
 });
 
+//* ------- Functions ------- */
+
 const createPcSelection = () => {
   const pcArr = ["rock", "paper", "scissor"];
   const pcRandom = pcArr[Math.floor(Math.random() * 3)];
   pcSelectImg.src = `./assets/${pcRandom}.png`;
   pcSelectImg.alt = pcRandom;
   pcChoiceDiv.appendChild(pcSelectImg);
+  calculateResult();
 };
 
+const calculateResult = () => {
+  // console.log(userSelectImg.alt)
+  // console.log(pcSelectImg.alt)
+
+  //? Esitlik durumu
+  if (userSelectImg.alt === pcRandom) {
+    draw();
+  } else {
+    if (userSelectImg.alt === "rock") {
+      pcRandom === "paper" ? youLost() : youWin();
+    } else if (userSelectImg.alt === "scissor") {
+      pcRandom === "rock" ? youLost() : youWin();
+    } else if (userSelectImg.alt === "paper") {
+      pcRandom === "scissor" ? youLost() : youWin();
+    }
+  }
+};
+
+const draw = () => {
+  messagePar.textContent = "Its a draw";
+  scoreCardSection.style.color = YELLOW;
+  messagePar.style.backgroundColor = YELLOW;
+};
+
+const youLost = () => {};
+
+const youWin = () => {};
+
+//! İlkel Yontem
 //? Resimler
 // const rockImg = document.getElementById("rock")
 // const paperImg = document.getElementById("paper")
@@ -53,5 +96,3 @@ const createPcSelection = () => {
 //   image.alt = "scissor"
 //   yourChoiceDiv.appendChild(image)
 // })
-
-//* ------- Functions ------- */
