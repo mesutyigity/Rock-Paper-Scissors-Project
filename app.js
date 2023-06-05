dfdaa2d39fb1e94a36b5adec2d8bb7f9c4c6348c
Mesut <yildirimmesutyigit@gmail.com>
2023-06-05 21:52:30 +0300

    new commit

--- app.js
+++ app.js
@@ -1,19 +1,57 @@
 //* ------ Selectors ------- */
-const rockImg = document.getElementById("rock");
-const paperImg = document.getElementById("paper");
-const scissorImg = document.getElementById("scissor");
+const selectionArticle = document.querySelector(".selection");
 
+//? Secilen elemanlarin tayicilari
 const yourChoiceDiv = document.getElementById("your-choice");
-//* ------- Variables ------- */
+const pcChoiceDiv = document.getElementById("pc-choice");
 
-let image = document.createElement("img");
+//* ------- Variables ------- */
+let userSelectImg = document.createElement("img");
+let pcSelectImg = document.createElement("img");
 
 //* ------- Event Listeners ------- */
-
-rockImg.addEventListener("click", () => {
-  image.src = "./assets/rock.png";
-  image.alt = "rock";
-
-  yourChoiceDiv.appendChild(image);
+selectionArticle.addEventListener("click", (e) => {
+  // console.log(e.target.id)
+  if (e.target.id) {
+    userSelectImg.src = `./assets/${e.target.id}.png`;
+    userSelectImg.alt = e.target.id;
+    yourChoiceDiv.appendChild(userSelectImg);
+    createPcSelection();
+  }
 });
+
+const createPcSelection = () => {
+  const pcArr = ["rock", "paper", "scissor"];
+  const pcRandom = pcArr[Math.floor(Math.random() * 3)];
+  pcSelectImg.src = `./assets/${pcRandom}.png`;
+  pcSelectImg.alt = pcRandom;
+  pcChoiceDiv.appendChild(pcSelectImg);
+};
+
+//? Resimler
+// const rockImg = document.getElementById("rock")
+// const paperImg = document.getElementById("paper")
+// const scissorImg = document.getElementById("scissor")
+
+// rockImg.addEventListener("click", () => {
+//   image.src = "./assets/rock.png"
+//   image.alt = "rock"
+//   yourChoiceDiv.appendChild(image)
+
+//   //? innerHTML
+//   // yourChoiceDiv.innerHTML = `<img src="./assets/rock.png" alt="rock">`
+// })
+
+// paperImg.addEventListener("click", () => {
+//   image.src = "./assets/paper.png"
+//   image.alt = "paper"
+//   yourChoiceDiv.appendChild(image)
+// })
+
+// scissorImg.addEventListener("click", () => {
+//   image.src = "./assets/scissor.png"
+//   image.alt = "scissor"
+//   yourChoiceDiv.appendChild(image)
+// })
+
 //* ------- Functions ------- */
