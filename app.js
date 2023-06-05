f7f7ed8a93ab4dde8b8f78c3850cd909212351cf
Mesut <yildirimmesutyigit@gmail.com>
2023-06-05 20:57:21 +0300

    new commit

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 //* ------ Selectors ------- */
+const rockImg = document.getElementById("rock");
+const paperImg = document.getElementById("paper");
+const scissorImg = document.getElementById("scissor");
 
+const yourChoiceDiv = document.getElementById("your-choice");
 //* ------- Variables ------- */
 
+let image = document.createElement("img");
 
 //* ------- Event Listeners ------- */
 
+rockImg.addEventListener("click", () => {
+  image.src = "./assets/rock.png";
+  image.alt = "rock";
 
+  yourChoiceDiv.appendChild(image);
+});
 //* ------- Functions ------- */
